2a5b8f103db77fa0d39f726c0e705cdcd747d77b
Anuskota <anamangada.dev@gmail.com>
2024-01-26 09:59:49 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Router from "./configuration/Router.jsx";
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/configuration/Router.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home />}></Route> */}
+       
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
